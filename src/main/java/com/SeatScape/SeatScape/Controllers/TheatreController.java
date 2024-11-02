33cdb88a9519de0e8ef317dd3ca3630fba622e72
Amaan Ali Khan <amaanali.amaanali@gmail.com>
2024-11-02 16:51:02 +0530

    CRUD operations for Theatre

--- src/main/java/com/SeatScape/SeatScape/Controllers/TheatreController.java
+++ src/main/java/com/SeatScape/SeatScape/Controllers/TheatreController.java
@@ -1,7 +1,80 @@
 package com.SeatScape.SeatScape.Controllers;
 
+import com.SeatScape.SeatScape.Entities.Theatre;
+import com.SeatScape.SeatScape.Services.TheatreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 
-
+@RestController
+@RequestMapping("/theatre")
 public class TheatreController {
+
+    @Autowired
+    private TheatreService theatreService;
+
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        List<Theatre> all = theatreService.getAll();
+        if (all != null && !all.isEmpty()) {
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(all, HttpStatus.NOT_FOUND);
+
+    }
+
+    @GetMapping("theatreId/{theatreId}")
+    public ResponseEntity<?> getEntryById(@PathVariable Long theatreId) {
+        Optional<Theatre> f = theatreService.findById(theatreId);
+
+        if (f.isPresent()) {
+            return new ResponseEntity<>(f, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(f, HttpStatus.NOT_FOUND);
+
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> createEntry(@RequestBody Theatre theatre) {
+
+        try {
+            theatreService.saveEntry(theatre);
+            return new ResponseEntity<>(theatre, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        }
+    }
+
+
+    @DeleteMapping("theatreId/{theatreId}")
+    public ResponseEntity<?> DeleteById(@PathVariable Long theatreId) {
+        theatreService.deleteById(theatreId);
+        return new ResponseEntity<>(theatreService, HttpStatus.NO_CONTENT);
+    }
+
+
+    @PutMapping("theatreId/{theatreId}")
+    public ResponseEntity<?> updateById(@PathVariable Long theatreId, @RequestBody Theatre newEntry) {
+        Theatre old = theatreService.findById(theatreId).orElse(null);
+        if (old != null) {
+            old.setTheatreName(newEntry.getTheatreName() != null && !newEntry.getTheatreName().equals("") ? newEntry.getTheatreName() : old.getTheatreName());
+            old.setLocation(newEntry.getLocation() != null && !newEntry.getLocation().equals("") ? newEntry.getLocation() : old.getLocation());
+
+
+            old.setSeatsLayout(newEntry.getSeatsLayout() != null ? newEntry.getSeatsLayout() : old.getSeatsLayout());
+
+            theatreService.saveEntry(old);
+            return new ResponseEntity<>(old, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
--- src/main/java/com/SeatScape/SeatScape/Services/TheatreService.java
+++ src/main/java/com/SeatScape/SeatScape/Services/TheatreService.java
@@ -1,4 +1,39 @@
 package com.SeatScape.SeatScape.Services;
 
+import com.SeatScape.SeatScape.Entities.Theatre;
+import com.SeatScape.SeatScape.Repositories.TheatreRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TheatreService {
+
+    @Autowired
+    private TheatreRepo theatreRepo;
+
+    // Create
+    public void saveEntry(Theatre theatre){
+        theatreRepo.save(theatre);
+    }
+
+    // Read
+    public List<Theatre> getAll(){
+        return theatreRepo.findAll();
+    }
+
+    // Read By id
+    public Optional<Theatre> findById(Long theatreId){
+        return theatreRepo.findById(theatreId);
+    }
+    // Update By id
+
+    // Delete By id
+    public void deleteById(Long theatreId){
+        theatreRepo.deleteById(theatreId);
+    }
+
+
 }
